aec467c671167bcb12ab2364b3b0fd32558a5fae
Baron-Paelen <ayegiaya@ucsc.edu>
2025-08-14 19:08:04 -0700

    initial conversion done, gotta test on windows now

--- arduino_helper.py
+++ arduino_helper.py
@@ -1,276 +1,122 @@
-import pyduinocli
-import re
-import subprocess
+from pyduinocli.commands.arduino import ArduinoCliCommand as Arduino
 import os
-import requests
 from pprint import pprint
 from zipfile import ZipFile
 
 
-cli_path = 'NOT SET'
-exe_path = 'NOT SET'
-if os.name == 'nt':
-    cli_path = os.path.join('.', 'arduino-cli', 'windows')
-    exe_path = os.path.join(cli_path, 'arduino-cli.exe')
-elif os.name == 'posix':
-    cli_path = os.path.join('.', 'arduino-cli', 'mac_linux')
-    exe_path = os.path.join(cli_path, 'arduino-cli')
-    
+# cli_path = 'NOT SET'
+# exe_path = 'NOT SET'
+# if os.name == 'nt':
+#     cli_path = os.path.join('.', 'arduino-cli', 'windows')
+#     exe_path = os.path.join(cli_path, 'arduino-cli.exe')
+# elif os.name == 'posix':
+#     cli_path = os.path.join('.', 'arduino-cli', 'mac_linux')
+#     exe_path = os.path.join(cli_path, 'arduino-cli')
 
-
-
-# installing the arduino-cli
-def install_arduino_cli():
-    # check if the arduino-cli is installed
-    try:
-        vercmd = os.path.join(cli_path, 'arduino-cli.exe') + ' version'
-        subprocess.run(vercmd, shell=True, check=True)
-    except subprocess.CalledProcessError:
-        # install the arduino-cli
-        # if windows, download the cli exe
-        if os.name == 'nt':
-            print("Downloading Windows arduino-cli...")
-            
-            try:
-                os.mkdir(os.path.join('.', 'arduino-cli'))
-                os.mkdir(os.path.join('.', 'arduino-cli', 'windows'))
-            except:
-                print('folder exists!')
-                raise
-                exit(1)
-            
-            url = "https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Windows_64bit.zip"
-            
-            r = requests.get(url)
-            with open(os.path.join(cli_path, "arduino-cli.zip"), "wb") as f:
-                f.write(r.content)
-
-            # extract the zip file
-            with ZipFile(os.path.join(cli_path, "arduino-cli.zip"), 'r') as zip_ref:
-                zip_ref.extractall(cli_path)
-            
-            # remove the zip file and license
-            os.remove(os.path.join(cli_path, "arduino-cli.zip"))
-            os.remove(os.path.join(cli_path, "LICENSE.txt"))
-        elif os.name == 'posix':
-            print("Downloading Mac/Linux arduino-cli...")
-
-            try:
-                os.mkdir(os.path.join('.', 'arduino-cli'))
-                os.mkdir(os.path.join('.', 'arduino-cli', 'mac_linux'))
-            except:
-                print('folder exists!')
-                return
-            
-            posixcmd = 'curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | BINDIR=./arduino-cli/mac_linux sh'
-            
-            # run the command
-            subprocess.run(posixcmd, shell=True, check=True)
-        else:
-            print("OS not supported")
-            print("How did you get here?")
-            return
+class BoardStruct:
+    def __init__(self, name:str = None, fqbn:str = None, port:str = None, sn:str = None):
+        self.__name = name
+        self.__fqbn = fqbn
+        self.__port = port
+        self.__serial_number = sn
         
-# run the arduino-cli with the given arguments
-def run_arduino_cli(args: list[str]):
-    cmd = exe_path + ' '
-
-    cmd += '--additional-urls https://adafruit.github.io/arduino-board-index/package_adafruit_index.json '
+    @property
+    def name(self):
+        return self.__name
     
-    for arg in args:
-        cmd += arg + ' '
+    @property
+    def fqbn(self):
+        return self.__fqbn
+    
+    @property
+    def port(self):
+        return self.__port
+    
+    @property
+    def serial_number(self):
+        return self.__serial_number
+    
+    @property
+    def hardware_id(self):
+        return self.__serial_number
+    
+    def __repr__(self):
+        return (
+            f"Name: {self.name}\n"
+            f"FQBN: {self.fqbn}\n"
+            f"Port: {self.port}\n"
+            f"Serial Number: {self.serial_number}"
+        )
+    
+class ExtendoArduino(Arduino):
+    # install necessary cores (idk what theyre actually called) for the M0 board
+    # returns output of the command ig
+    def install_cores(self):
+        cores = [
+            'adafruit:samd',
+            'arduino:samd',
+        ]
+        return self.core.install(cores)
+    
+    # install libraries
+    def install_default_libs(self):
+        libs = [
+            'RTCZero',
+            'SdFat - Adafruit Fork',
+            'Adafruit TinyUSB Library',
+        ]
         
-    try :
-        return subprocess.run(cmd, text=True, shell=True, check=True, capture_output=True)
-    except subprocess.CalledProcessError as e:
-        print("Error running command: " + cmd)
-        print("Error code:\n" + e.stderr)
+        return self.lib.install(libs)
+
+    # compile, upload, and verify the sketch at the given path (path should be the .ino file)
+    def compile_upload_verify(self, port: str, fqbn: str, sketch_path: str, usbstack='arduino'):
+        if not os.path.exists(sketch_path):
+            raise FileNotFoundError(f"Sketch '{sketch_path}'doesn't exist!")
         
-# compile, upload, and verify the sketch at the given path (path should be the .ino file)
-def compile_upload_verify(port: str, fqbn: str, sketch_path: str, usbstack='arduino'):
-    # verify the path is valid
-    if not os.path.exists(sketch_path):
-        print("Invalid path")
-        # exit(1)
+        return self.compile(
+            sketch=sketch_path, 
+            port=port, 
+            fqbn=f"{fqbn}:usbstack={usbstack}",
+            verify=True,
+            upload=True,
+            clean=True
+        )
+
+    # returns a list of tuples of the port, FQBN, and core of all connected boards
+    # TODO make this function actually useful - maybe fully extract FQBN and Board Name and Port
+    def get_board_data(self):
+        boards = []
+        raw_boards = self.board.list()['result']['detected_ports']
+        
+        for board in raw_boards:
+            name = board["matching_boards"][0]["name"]
+            fqbn = board["matching_boards"][0]["fqbn"]
+            port = board["port"]["address"]
+            sn   = board["port"]["hardware_id"]
+
+            boards.append(BoardStruct(name, fqbn, port, sn))
+
+        return boards
+
+    
+        
+# def compile_upload_verify(port: str, fqbn: str, sketch_path: str, usbstack='arduino'):
+#     # verify the path is valid
+#     if not os.path.exists(sketch_path):
+#         print("Invalid path")
+#         # exit(1)
         
     
-    # shaboom
-    result = run_arduino_cli(['compile', '-p', port, '-b', fqbn + ':usbstack=' + usbstack, sketch_path, '-u', '-t', '--clean'])
+#     # shaboom
+#     result = run_arduino_cli(['compile', '-p', port, '-b', fqbn + ':usbstack=' + usbstack, sketch_path, '-u', '-t', '--clean'])
     
-    return result
+#     return result
 
-# install necessary cores (idk what theyre actually called) for the M0 board
-# returns output of the command ig
-def install_cores(arduino: pyduinocli.commands.arduino.ArduinoCliCommand):
-    cores = [
-        'adafruit:samd',
-        'arduino:samd',
-    ]
-    return arduino.core.install(cores)
-    
-# def install_cores():
-#     out = []
-    
-#     # install the Adafruit SAMD board library
-#     out.append(run_arduino_cli(['core', 'install', 'adafruit:samd']))
-    
-#     # install the Adafruit AVR board library
-#     out.append(run_arduino_cli(['core', 'install', 'adafruit:avr']))
-    
-#     # install the Arduino SAMD board library
-#     out.append(run_arduino_cli(['core', 'install', 'arduino:samd']))
-
-#     # install the Arduino SAM board library
-#     out.append(run_arduino_cli(['core', 'install', 'arduino:sam']))
-    
-#     # install the Arduino AVR board library
-#     out.append(run_arduino_cli(['core', 'install', 'arduino:avr']))
-
-#     return out
-
-# install libraries
-def install_default_libs(arduino: pyduinocli.commands.arduino.ArduinoCliCommand):
-    libs = [
-        'RTCZero',
-        'SdFat - Adafruit Fork',
-        'Adafruit TinyUSB Library',
-    ]
-    
-    return arduino.lib.install(libs)
-# def install_default_libs():
-#     out = []
-    
-#     # install the RTCZero library
-#     out.append(run_arduino_cli(['lib', 'install', 'RTCZero']))
-
-#     # install the Adafruit NeoPixel library
-#     out.append(run_arduino_cli(['lib', 'install', '"Adafruit NeoPixel"']))
-    
-#     # install the Adafruit SPIFlash library
-#     out.append(run_arduino_cli(['lib', 'install', '"Adafruit SPIFlash"']))
-    
-#     # install the Adafruit SdFat library
-#     out.append(run_arduino_cli(['lib', 'install', '"SdFat - Adafruit Fork"']))
-    
-#     # install the Adafruit TinyUSB library
-#     out.append(run_arduino_cli(['lib', 'install', '"Adafruit TinyUSB Library"']))
-    
-#     # install the Adafruit ZeroDMA library
-#     out.append(run_arduino_cli(['lib', 'install', '"Adafruit Zero DMA Library"']))
-    
-#     # install the Adafruit LibPrintf library
-#     out.append(run_arduino_cli(['lib', 'install', '"LibPrintf"']))
-
-#     return out
-
-# returns a list of tuples of the port, FQBN, and core of all connected boards
-def get_board_data(arduino: pyduinocli.commands.arduino.ArduinoCliCommand):
-    return arduino.board.list()
-# data may be imperfect so must be checked
-# def get_board_data():
-#     # check if a string is a valid looking FQBN
-#     def is_fqbn_valid(fqbn: str):
-#         # Define a regular expression pattern for the desired format
-#         pattern = r'^[^\s:]+:[^\s:]+:[^\s:]+$'
-
-#         # Use re.match to check if the input string matches the pattern
-#         match = re.match(pattern, fqbn)
-        
-#         return None if match is None else match.group()
-        
-#     # check if a string is a valid looking core
-#     def is_core_valid(core: str):
-#         # Define a regular expression pattern for the desired format
-#         pattern = r'^[^\s:]+:[^\s:]+$'
-
-#         # Use re.match to check if the input string matches the pattern
-#         match = re.match(pattern, core)
-        
-#         return None if match is None else match.group()
-
-
-#     # get the list of connected boards
-#     board_list_data = run_arduino_cli(['board', 'list'])
-
-#     # split the output by newlines
-#     entries = board_list_data.stdout.split('\n')
-    
-#     # make sure there's at least 1 connected board
-#     if len(entries) < 2:
-#         return None
-    
-#     # entries[0] is the header/labels
-#     entries = entries[1:]
-#     grabbed_data = []
-    
-#     for entry in entries:
-    
-#         port, FQBN, core = None, None, None
-        
-#         # split the entry by spaces (gets messy)
-#         entry = entry.split(' ')
-        
-#         # find the port (its the first entry lol)
-#         # looks like "COM3" or "/dev/ttyACM0"
-#         port = entry[0]
-        
-#         # find the FQBN (it should look like "xxx:yyy:zzz")
-#         for j in entry:
-#             if is_fqbn_valid(j) is not None:
-#                 FQBN = j
-#                 break
-            
-#         # find the core (it should look like "xxx:yyy")
-#         for j in entry:
-#             if is_core_valid(j) is not None:
-#                 core = j
-#                 break
-            
-#         grabbed_data.append((port, FQBN, core))
-
-#     return grabbed_data
-    
+ 
     
 
 if __name__ == '__main__':
-    # install the arduino-cli, board reqs, and lib reqs if not installed
-    ardu = pyduinocli.Arduino(additional_urls=['https://adafruit.github.io/arduino-board-index/package_adafruit_index.json'])
-    # pprint(ardu.core.list())
-    out = install_cores(ardu)
-    pprint(out)
-    print('M0 reqs installed')
-    
-    # pprint(ardu.lib.list())
-    out = install_default_libs(ardu)
-    pprint(out)
-    print('Default libs installed')
-    
-
-    boards = get_board_data(ardu)['result']['detected_ports'][0]['matching_boards']
-    print('found boards:')
-    for n, board_dict in enumerate(boards):
-        print(f"{n}:")
-        for k, v in board_dict.items():
-            print(f"  {k}: {v}")
-        print()
-
-    # # pick the right board manually :(
-    # board_num = int(input('Enter the number of the board to use: '))
-    # port, FQBN, core = boards[board_num]
-    
-    # print("Port: " + port)
-    # print("FQBN: " + FQBN)
-    # print("Core: " + core)
-    # print('---------------------------------')
-
-    # # compile_upload_verify(port, FQBN, '"C:\\Users\\SEVAK\\Documents\\GitHub\\IRIS-Project\\sandbox\\M0\\mass storage andrew\\msc_sdfat\\msc_sdfat.ino"', usbstack='tinyusb')
-    # # compile_upload_verify(port, FQBN, '"/home/paelen/Documents/GitHub/IRIS-Project/sandbox/M0/SdFat/datalogger_tAv_bin/datalogger_tAv_bin.ino"', usbstack='tinyusb')
-    # # compile_upload_verify(port, FQBN, '"/home/paelen/paelen.ino"')
-    
-    # while True:
-    #     # get input and split by space into list
-    #     args = input("Enter command: ").split(' ')
-    #     print(run_arduino_cli(args).stdout)
+    pass
     
     
     
--- main.py
+++ main.py
@@ -1,14 +1,58 @@
 import pyduinocli
+import arduino_helper as a_h
 from pprint import pprint
 
 def main():
-    print("Hello from iris-gui!")   
-    cli = pyduinocli.Arduino(additional_urls=['https://adafruit.github.io/arduino-board-index/package_adafruit_index.json'])
-    print(type(cli))
-    # pprint(cli.version()['result'])
+    # install the arduino-cli, board reqs, and lib reqs if not installed
+    ardu = a_h.ExtendoArduino(additional_urls=['https://adafruit.github.io/arduino-board-index/package_adafruit_index.json'])
+    out = ardu.install_cores()
+    print('M0 reqs installed')
     
-    pprint(cli.board.list()['result']['detected_ports'][0])
+    
+    # pprint(ardu.lib.list())
+    out = ardu.install_default_libs()
+    print('Default libs installed')
+    
 
+    boards = ardu.get_board_data()
+    print('found boards:')
+    for n, board_deets in enumerate(boards):
+        print('---------------------------------')
+        print(f"{n}:")
+        print(board_deets)
+        print('---------------------------------')
+        print()
+
+    # pick the right board manually :
+    board_num = int(input('Enter the number of the board to use: '))
+    sel_board = boards[board_num]
+    # port, FQBN, core = boards[board_num]
+    
+    print("Selected Board:")
+    print('---------------------------------')
+    print(sel_board)
+    print('---------------------------------')
+    
+
+    da_path = "/home/paelen/Documents/GitHub/IRIS-Project/packages/M0/Binary Serial Logger/serial_log/serial_log.ino"
+    
+    # import tkinter as tk
+    # from tkinter import filedialog
+    # root = tk.Tk()
+    # root.withdraw()
+    # da_path = filedialog.askopenfilename()
+
+    # # compile_upload_verify(port, FQBN, '"C:\\Users\\SEVAK\\Documents\\GitHub\\IRIS-Project\\sandbox\\M0\\mass storage andrew\\msc_sdfat\\msc_sdfat.ino"', usbstack='tinyusb')
+    # # compile_upload_verify(port, FQBN, '"/home/paelen/Documents/GitHub/IRIS-Project/sandbox/M0/SdFat/datalogger_tAv_bin/datalogger_tAv_bin.ino"', usbstack='tinyusb')
+    # # compile_upload_verify(port, FQBN, '"/home/paelen/paelen.ino"')
+    out = ardu.compile_upload_verify(
+        port=sel_board.port,
+        fqbn=sel_board.fqbn,
+        sketch_path=da_path
+    )
+    
+    pprint(out)
+    
 
 if __name__ == "__main__":
     main()
