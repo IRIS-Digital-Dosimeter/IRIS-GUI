4b4659b6d481b187a28b9e28283e6cc91e0250eb
Baron-Paelen <50848243+Baron-Paelen@users.noreply.github.com>
2025-08-19 15:36:29 -0700

    temp file selection dialog created. must test on linux

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pyduinocli
 import arduino_helper as a_h
+import ast
+import crossfiledialog
+from pathlib import Path
 from pprint import pprint
 
 def main():
@@ -34,24 +37,29 @@ def main():
     print('---------------------------------')
     
 
-    da_path = "/home/paelen/Documents/GitHub/IRIS-Project/packages/M0/Binary Serial Logger/serial_log/serial_log.ino"
+    # da_path = "/home/paelen/Documents/GitHub/IRIS-Project/packages/M0/Binary Serial Logger/serial_log/serial_log.ino"
+    # da_path = "C:\\Users\\Sevak\\Documents\\GitHub\\IRIS-Project\\packages\\M0\\Binary Serial Logger\\serial_log\\serial_log.ino"
+
+    da_path = Path(crossfiledialog.open_file(title="Select an Arduino sketch (.ino) file", filter="*.ino")).resolve()
+    # da_path = Path("C:\\Users\\Sevak\\Documents\\GitHub\\IRIS-Project\\packages\\M0\\Binary Serial Logger\\serial_log\\serial_log.ino").resolve()
     
-    # import tkinter as tk
-    # from tkinter import filedialog
-    # root = tk.Tk()
-    # root.withdraw()
-    # da_path = filedialog.askopenfilename()
+    print(f"DA PATH: {da_path}")
 
     # # compile_upload_verify(port, FQBN, '"C:\\Users\\SEVAK\\Documents\\GitHub\\IRIS-Project\\sandbox\\M0\\mass storage andrew\\msc_sdfat\\msc_sdfat.ino"', usbstack='tinyusb')
     # # compile_upload_verify(port, FQBN, '"/home/paelen/Documents/GitHub/IRIS-Project/sandbox/M0/SdFat/datalogger_tAv_bin/datalogger_tAv_bin.ino"', usbstack='tinyusb')
     # # compile_upload_verify(port, FQBN, '"/home/paelen/paelen.ino"')
-    out = ardu.compile_upload_verify(
-        port=sel_board.port,
-        fqbn=sel_board.fqbn,
-        sketch_path=da_path
-    )
-    
-    pprint(out)
+    try:
+        out = ardu.compile_upload_verify(
+            port=sel_board.port,
+            fqbn=sel_board.fqbn,
+            sketch_path=da_path.as_posix()
+        )
+        pprint(out)
+    except pyduinocli.errors.arduinoerror.ArduinoError as e:
+        print("Error during compilation/upload/verification:")
+        d = ast.literal_eval(str(e))
+        pprint(d['__stderr'])
+        return
     
 
 if __name__ == "__main__":
