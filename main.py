fe3aa7370439ad16e65a7e6312d9c1e286333e48
Baron-Paelen <ayegiaya@ucsc.edu>
2025-09-13 15:11:48 -0700

    dev helper main.py

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 import pyduinocli
 import arduino_helper as a_h
-import ast
+import sys
+import subprocess
 import crossfiledialog
 from pathlib import Path
 from pprint import pprint
 
 def main():
     
-    # print(f"main.py directory: {a_h.ExtendoArduino.LIB_DIR}")
-    # print(f"defaault cli tool dir: {a_h.ExtendoArduino.DEFAULT_CLI_TOOL_DIR}")
-    # print(f"cli yaml path:         {a_h.ExtendoArduino.CLI_YAML_PATH}")
-    # print(f"cli data path:         {a_h.ExtendoArduino.CLI_DATA_PATH}")
-    # print(f"cli user path:         {a_h.ExtendoArduino.CLI_USER_PATH}")
     
     # install the arduino-cli, board reqs, and lib reqs if not installed
     # also sets up the config file usage and assigns the proper working directories for the CLI tool
@@ -20,50 +16,114 @@ def main():
         timeout='600s'
     )
     
-    out = ardu.install_cores()
-    print('M0 reqs installed')
+    # out = ardu.install_cores()
+    # print('M0 reqs installed')
     
-    # install the default libraries
-    out = ardu.install_default_libs()
-    print('Default libs installed')
+    # # install the default libraries
+    # out = ardu.install_default_libs()
+    # print('Default libs installed')
     
     # get the list of boards and print them
     boards = ardu.get_board_data()
-    print('found boards:')
-    for n, board_deets in enumerate(boards):
-        print('---------------------------------')
-        print(f"{n}:")
-        print(board_deets)
-        print('---------------------------------')
-        print()
+    # print('found boards:')
+    # for n, board_deets in enumerate(boards):
+    #     print('---------------------------------')
+    #     print(f"{n}:")
+    #     print(board_deets)
+    #     print('---------------------------------')
+    #     print()
 
-    # pick the right board manually :
-    board_num = int(input('Enter the number of the board to use: '))
-    sel_board = boards[board_num]
-    # port, FQBN, core = boards[board_num]
+    # # pick the right board manually :
+    # board_num = int(input('Enter the number of the board to use: '))
+    # sel_board = boards[board_num]
+    # # port, FQBN, core = boards[board_num]
     
-    print("Selected Board:")
-    print('---------------------------------')
-    print(sel_board)
-    print('---------------------------------')
+    # print("Selected Board:")
+    # print('---------------------------------')
+    # print(sel_board)
+    # print('---------------------------------')
     
 
     # da_path = "/home/paelen/Documents/GitHub/IRIS-Project/packages/M0/Binary Serial Logger/serial_log/serial_log.ino"
     # da_path = "C:\\Users\\Sevak\\Documents\\GitHub\\IRIS-Project\\packages\\M0\\Binary Serial Logger\\serial_log\\serial_log.ino"
 
-    da_path = Path(crossfiledialog.open_file(title="Select an Arduino sketch (.ino) file", filter="*.ino")).resolve()
+    # da_path = Path(crossfiledialog.open_file(title="Select an Arduino sketch (.ino) file", filter="*.ino")).resolve()
     # da_path = Path("C:\\Users\\Sevak\\Documents\\GitHub\\IRIS-Project\\packages\\M0\\Binary Serial Logger\\serial_log\\serial_log.ino").resolve()
+    sketches = {
+        "serial": (Path.home() / "Documents" / "GitHub" / "IRIS-Project" / "packages" / "M0" / "Binary Serial Logger" / "serial_log" / "serial_log.ino").resolve(),
+        "tri": (Path.home() / "Documents" / "GitHub" / "IRIS-Project" / "packages" / "M0" / "Triangle Wave Generator" / "triangle_wave_generator" / "triangle_wave_generator.ino").resolve(),
+        "logger": (Path.home() / "Documents" / "GitHub" / "IRIS-Project" / "packages" / "M4" / "M4 Datalogger" / "dma_dual_adc_unified_SdFat" / "dma_dual_adc_unified_SdFat.ino").resolve(),
+    }
     
+    if sys.argv[1] not in sketches.keys():
+        print("invalid sketch")
+        exit()
+    
+    des_sketch = sketches[sys.argv[1]].as_posix()
 
-    # # compile_upload_verify(port, FQBN, '"C:\\Users\\SEVAK\\Documents\\GitHub\\IRIS-Project\\sandbox\\M0\\mass storage andrew\\msc_sdfat\\msc_sdfat.ino"', usbstack='tinyusb')
-    # # compile_upload_verify(port, FQBN, '"/home/paelen/Documents/GitHub/IRIS-Project/sandbox/M0/SdFat/datalogger_tAv_bin/datalogger_tAv_bin.ino"', usbstack='tinyusb')
-    # # compile_upload_verify(port, FQBN, '"/home/paelen/paelen.ino"')
-    out = ardu.compile_upload_verify(
-        port=sel_board.port,
-        fqbn=sel_board.fqbn,
-        sketch_path=da_path.as_posix()
-    )
-    pprint(out)
+
+    if sys.argv[1] == "serial":
+        if "Adafruit Feather M4 Express (SAMD51)" not in [b.name for b in boards]:
+            print("AH")
+            exit()
+
+
+        for b in boards:
+            if b.name == "Adafruit Feather M4 Express (SAMD51)":
+                sel_board = b
+        
+        out = ardu.compile_upload_verify(
+            port=sel_board.port,
+            fqbn=sel_board.fqbn,
+            sketch_path=des_sketch
+        )
+        cmd = [
+            sys.executable,
+            (Path.home() / "Documents" / "GitHub" / "IRIS-Project" / "packages" / "M0" / "Binary Serial Logger" / "serial_importer_all_in_one" / "serial_importer_all_in_one.py").as_posix(),
+            "--plot",
+            f"port={sel_board.port}",
+            "baud=9600",
+            "sec=20",
+            "hz=10",
+        ]
+        print(" ".join(cmd))
+        result = subprocess.run(cmd)
+
+        print("STDOUT:\n", result.stdout)
+        print("STDERR:\n", result.stderr)
+        
+        
+    if sys.argv[1] == "tri":
+        if "Adafruit Feather M0 (SAMD21)" not in [b.name for b in boards]:
+            exit()
+        
+        for b in boards:
+            if b.name == "Adafruit Feather M0 (SAMD21)":
+                sel_board = b
+    
+        out = ardu.compile_upload_verify(
+            port=sel_board.port,
+            fqbn=sel_board.fqbn,
+            sketch_path=des_sketch
+        )
+        print("tri uploaded")
+        
+    if sys.argv[1] == "logger":
+        if "Adafruit Feather M4 Express (SAMD51)" not in [b.name for b in boards]:
+            exit()
+        
+        for b in boards:
+            if b.name == "Adafruit Feather M4 Express (SAMD51)":
+                sel_board = b
+    
+        out = ardu.compile_upload_verify(
+            port=sel_board.port,
+            fqbn=sel_board.fqbn,
+            sketch_path=des_sketch
+        )
+        
+    # print(out)
+    
     # try:
     #     out = ardu.compile_upload_verify(
     #         port=sel_board.port,
@@ -79,5 +139,6 @@ def main():
     
 
 if __name__ == "__main__":
+    print(sys.argv)
     main()
 
---
+++ sketch_handler.py
@@ -0,0 +1,106 @@
+import sys
+import os
+import arduino_helper as ah
+import tomllib
+import requests
+from dotenv import load_dotenv
+from pprint import pprint
+from pathlib import Path
+
+LOCKFILE_PATH = Path(__file__).parent / "iris_sketches" / "sketches.lock"
+load_dotenv()
+GH_TOKEN = os.getenv("GITHUB_PAT")
+
+def get_sketch(name, version="latest", owner="IRIS-Digital-Dosimeter", repo="IRIS-Project", branch: str = None):
+    try:
+        lock = load_lockfile(LOCKFILE_PATH)
+    except Exception:
+        raise Exception("Malformed TOML lockfile")
+
+    sketch_info = lock.get(name)
+    print("sketch info below: ")
+    pprint(sketch_info)
+    print()
+
+    if version == "latest":
+        if not online():
+            raise RuntimeError("Cannot fetch 'latest' while offline.")
+        commit = get_latest_commit(sketch_info["remote_path"], owner, repo, branch)
+    else:
+        commit = version  # explicit commit hash
+
+    
+    return commit
+
+    # # Not in lockfile → fetch
+    # if not sketch_info or sketch_info["commit"] != commit:
+    #     if not online() and not os.path.exists(f"./downloads/{name}"):
+    #         raise RuntimeError(f"{name}@{commit} not available offline.")
+    #     download_sketch(name, commit)
+    #     lock[name] = record_metadata(name, commit)
+    #     save_lockfile(lock)
+    # else:
+    #     print(f"Using cached {name}@{commit}")
+
+    # compile_and_upload(lock[name]["local_path"])
+
+    
+def load_lockfile(p: Path) -> dict | None:
+    with open(p.as_posix(), "rb") as f:
+        data = tomllib.load(f)
+        return data
+
+def online(timeout=2) -> bool:
+    try:
+        r = requests.head("https://api.github.com", timeout=timeout)
+        return r.status_code < 500
+    except requests.RequestException:
+        return False
+    
+def get_latest_commit(remote_path: str, owner: str, repo: str, branch: str = None) -> str | None:
+    # return None
+
+    req_params = {
+        "path": remote_path,
+        "pages": 1,
+        "per_page": 1,
+    }
+    if branch:
+        req_params['sha'] = branch
+        
+    try:
+        r = requests.get(
+            f"https://api.github.com/repos/{owner}/{repo}/commits",
+            data=req_params,
+            headers= {
+                "Authorization": "Bearer: {GH_TOKEN}",
+                "accept": "application/vnd.github+json",
+            }
+        )
+        return r.json()
+    except requests.RequestException:
+        return None
+
+
+if __name__ == "__main__":
+    
+    print('running main')
+    
+    arduino = ah.ExtendoArduino(
+        additional_urls=[
+            'https://adafruit.github.io/arduino-board-index/package_adafruit_index.json'
+        ]
+    )
+    
+    # load_lockfile(LOCKFILE_PATH)
+    print(f"github reachable? {online()}")
+    print()
+    commits = get_sketch("SerialLogger", branch="cleanup")
+    print(len(commits))
+    print()
+    print()
+    
+    for commit in commits:
+        # pprint(commit['commit'])
+        print(commit['commit']['message'])
+        print(commit['commit']['tree']['sha'])
