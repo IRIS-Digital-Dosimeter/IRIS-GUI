de4f55ffc2b6eeaf1247bbf3920ccda99940346b
Baron-Paelen <50848243+Baron-Paelen@users.noreply.github.com>
2025-12-01 22:13:27 -0800

    changed MD for intro screen

--- iris_screens/intro_screen.py
+++ iris_screens/intro_screen.py
@@ -13,10 +13,21 @@ class IntroScreen(Screen):
             yield Markdown(INTRO_MD)
 
 INTRO_MD = """\
-# Welcome to the IRIS GUI!
-
-## What is this?
-This is a simple GUI application to help you upload Arduino sketches to your board.
+# Screens
+The GUI is split into several "screens". You can switch between them with **CTRL+S**. You're also prompted at startup to choose a screen.
+## ManualUploadScreen
+Allows you to manually select a sketch to upload, and view the associated `config.h` file if the sketch supports a standard one. Note that the "config editor" tab is read-only. 
+## MDAScreen
+Preset sketches are included in the **MDAScreen**:
+- **Binary Serial Logger**
+	- Constantly dumps samples over the serial line.
+	- Can use the included **serial_importer_all_in_one** script to read the data.
+- **M4 Datalogger**
+	- Logs data continuously on 4 pins at high speeds to the SD card.
+- **Toggle Switch Datalogger**
+	- Uses a physical switch to enable/disable data logging on 4 pins at high speeds to the SD card.
+- **SD Card Exposer**
+	- Exposes the onboard SD card as a flash drive to the connected computer. Poor performance.
 
 
 """
